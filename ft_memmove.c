0828b8502eaf1b341576f5631b4b0183cfc56c22
sebbaall <sebaalsayed543@gmail.com>
2026-09-18 21:04:38 +0300

    library line deleted

--- ft_memmove.c
+++ ft_memmove.c
@@ -11,7 +11,6 @@
 /* ************************************************************************** */
 
 #include <stddef.h>
-#include <stdlib.h>
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
